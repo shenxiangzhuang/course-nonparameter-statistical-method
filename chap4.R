3a80b5559c8154f3ab3b05e3c8a69d50f4d9ab17
shenxiangzhuang <1021550072@qq.com>
2017-12-07 13:25:09 +0800

    chap4

--- chap4.R
+++ chap4.R
@@ -62,5 +62,62 @@ fact <- gl(4,1,80,labels=LETTERS[1:4])
 block <- gl(20,4,labels=letters[1:20])
 cochran.qtest(response~fact|block)
 
-# 4.8
+# 4.8 完全区组设计：page检验
+# Way1: concord
+library(concord)
+d <- read.table("/home/shensir/Documents/MyPrograming/R/NonparameterStat-Course-WIth-R/data/blead1.TXT")
+page.trend.test(d)
+
+# Way2:大样本近似
+rd <- apply(d, 1, rank)
+R <- apply(rd, 1, sum)
+L <- sum(R*1:length(R))
+k <- dim(d)[2]
+b <- dim(d)[1]
+m <- b*k*(k+1)^2/4
+s <- sqrt(b*(k^3-k)^2/144/(k-1))
+Z <- (L-m)/s
+pvalue <- pnorm(Z, lower.tail = F)
+
+
+# 4.9 不完全区组设计：Durbin检验
+
+# 没有打结的情况
+d <- read.table("/home/shensir/Documents/MyPrograming/R/NonparameterStat-Course-WIth-R/data/mater.TXT")
+k <- max(d[, 2])
+b <- max(d[, 3])
+t <- length(d[d[,3]==1,1])
+r <- length(d[d[,2]==1,1])
+R <- d
+for( i in 1:b) R[d[,3]==i,1]=rank(d[d[,3]==i,1])
+RV <- NULL
+for(i in 1:k) RV = c(RV, sum(R[R[,2]==i,1]))
+D <- 12*(k-1)/(r*k*(t^2-1))*sum((RV-r*(t+1)/2)^2)
+p.value <- pchisq(D, k-1, lower.tail = F)
+
+# 打结的情况
+A <- sum(R[,1]^2)
+C <- b*t*(t+1)^2/4
+D <- (k-1)*sum((RV-r*(t+1)/2)^2)/(A-C)
+p.value <- pchisq(D, k-1, lower.tail = F)
+
+
+# 课后习题
+# T1
+d <- read.table("/home/shensir/Documents/MyPrograming/R/NonparameterStat-Course-WIth-R/data/4.10.1.TXT")
+
+# K-W
+kruskal.test(d[, 1], d[, 2])
+
+# J-T
+# 按照中位数从小到大的顺序重新排列数据
+medians <- aggregate(V1~V2, d, median)
+orders <- order(medians$V1)
+rep.orders <- NULL
+for(i in orders){rep.orders <- c(rep.orders, 
+                                 rep(i, nrow(d[d$V2==i, ])))}
+ordered.d <- d[match(rep.orders, d$V2), ]
+
+library(clinfun)
+jonckheere.test(ordered.d[, 1], ordered.d[, 2], alternative = "increasing")
 
